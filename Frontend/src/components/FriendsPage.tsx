74a2538a36a0604247dc291c1226f71800eb5c91
GreysonFlippo1 <51556136+GreysonFlippo1@users.noreply.github.com>
2023-12-23 20:18:43 -0800

    Added more mock friends settings

--- Frontend/src/components/FriendsPage.tsx
+++ Frontend/src/components/FriendsPage.tsx
@@ -28,6 +28,13 @@ const FriendsPage = () => {
           >
             Remove Friend
           </button>
+          <button
+            className="settings-input remove-link-button delete-button"
+            type="button"
+            placeholder="User Name"
+          >
+            Block
+          </button>
         </div>
       </div>
       <div className="settings-row">
@@ -49,6 +56,13 @@ const FriendsPage = () => {
           >
             Remove Friend
           </button>
+          <button
+            className="settings-input remove-link-button delete-button"
+            type="button"
+            placeholder="User Name"
+          >
+            Block
+          </button>
         </div>
       </div>
       <div className="settings-row">
@@ -60,6 +74,93 @@ const FriendsPage = () => {
           + Add Friend
         </button>
       </div>
+
+    <div className="settins-pane-title" style={{marginTop: '80px'}}>
+      PENDING REQUESTS
+    </div>
+    <div className="settings-row">
+      <div className="settings-name">
+        Taylor Swift
+      </div>
+      <div className="settings-coloumns-container">
+        <div className="friends-profile-picture" />
+        <input
+          className="settings-input"
+          type="text"
+          placeholder="ac130veterans@gmail.com"
+          disabled={true}
+        />
+        <button
+          className="settings-input remove-link-button"
+          type="button"
+          placeholder="User Name"
+        >
+          Accept Request
+        </button>
+        <button
+          className="settings-input remove-link-button delete-button"
+          type="button"
+          placeholder="User Name"
+        >
+          Block
+        </button>
+      </div>
+    </div>
+
+    <div className="settings-row">
+      <div className="settings-name">
+        DannyD
+      </div>
+      <div className="settings-coloumns-container">
+        <div className="friends-profile-picture" />
+        <input
+          className="settings-input"
+          type="text"
+          placeholder="ac130veterans@gmail.com"
+          disabled={true}
+        />
+        <button
+          className="settings-input remove-link-button delete-button"
+          type="button"
+          placeholder="User Name"
+        >
+          Cancel Request
+        </button>
+        <button
+          className="settings-input remove-link-button delete-button"
+          type="button"
+          placeholder="User Name"
+        >
+          Block
+        </button>
+      </div>
+    </div>
+
+  <div className="settins-pane-title" style={{marginTop: '80px'}}>
+    BLOCKED USERS
+  </div>
+  <div className="settings-row">
+    <div className="settings-name">
+      Kanye West
+    </div>
+    <div className="settings-coloumns-container">
+      <div className="friends-profile-picture" />
+      <input
+        className="settings-input"
+        type="text"
+        placeholder="ac130veterans@gmail.com"
+        disabled={true}
+      />
+      <button
+        className="settings-input remove-link-button"
+        type="button"
+        placeholder="User Name"
+      >
+        Unblock
+      </button>
+    </div>
+  </div>
+
     </div>
   );
 };
